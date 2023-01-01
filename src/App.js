3dc329e2b6005fa49abab8be4ccbaed7bdfad131
Opix <opixgamer@gmail.com>
2023-01-01 03:40:56 +0100

    false

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import data from'./data/data.json'
 
 function App() {
+	const name = data.name
+	console.log(name)
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload.<br/>
+					{data.companyName}
         </p>
+
         <a
           className="App-link"
           href="https://reactjs.org"
